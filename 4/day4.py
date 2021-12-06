c2efc335c19cd8621a53153e18c0930a9bb4e839
Sam Bukowski <sam.c.bukowski@gmail.com>
2021-12-05 18:10:10 -0700

    Day 4

--- 4/day4.py
+++ 4/day4.py
@@ -17,11 +17,15 @@ class BingoBoard:
         for i in range(0,5):
             output += "[ "
             for j in range(0,5):
-                hit = ("x" if self.data[i][j][1] == True else "o")
+                hit = ("x" if (self.data[i][j][1] == True) else "o")
                 output += f"({self.data[i][j][0]:2.0f}, {hit}) "
 
             output += "]\n"
-        output += "\n"
+        output += "Is "
+        if not self.check_winner():
+            output += "not "
+        output += "a winner.\n"
+        # output += "\n"
         return output
 
     def check_winner(self):
@@ -29,33 +33,37 @@ class BingoBoard:
         hit_count = 0
         for i in range(0,5):
             for j in range(0,5):
-                if self.data[i][j][1] == True:
+                if (self.data[i][j][1] == True):
                     hit_count += 1
                 if hit_count == 5:
                     return True
+            hit_count = 0
         
         # check columns
         hit_count = 0
         for i in range(0,5):
             for j in range(0,5):
-                if self.data[j][i][1] == True:
+                if (self.data[j][i][1] == True):
                     hit_count += 1
                 if hit_count == 5:
                     return True
+            hit_count = 0
 
         # check diags 
         hit_count = 0
         for i in range(0,5):
-            if self.data[i][i][1] == True:
+            if (self.data[i][i][1] == True):
                 hit_count += 1
-            if hit_count == 5:
-                return True
+        if hit_count == 5:
+            return True
+        hit_count = 0
 
         for i in range(0,5):
-            if self.data[i][4-i][1] == True:
+            if (self.data[i][4-i][1] == True):
                 hit_count += 1
-            if hit_count == 5:
-                return True
+        if hit_count == 5:
+            return True
+        hit_count = 0
 
         # else the board hast won yet
         return False
@@ -68,22 +76,37 @@ class BingoBoard:
                     if self.data[i][j][1] == False:
                         score += self.data[i][j][0]
 
-            return (score * val)
+            return (score * val), score
         else:
             return None
 
     def mark_number(self, number):
         for i in range(0,5):
             for j in range(0,5):
-                if self.data[i][j][0] == number:
+                if self.data[i][j][0] == number and self.data[i][j][1] == False:
                     self.data[i][j][1] = True
         return 
+
+    def calculate_win(self, inputs):
+        number_of_moves_needed = 0
+        winning_val = None
+        winning_score = None
+
+        for val in inputs:
+            self.mark_number( val )
+            number_of_moves_needed += 1
+            if self.check_winner() == True:
+                winning_val = val
+                winning_score = self.get_winning_score( val )
+                break
+
+        self.number_of_moves_needed = number_of_moves_needed
+        self.winning_val = winning_val
+        self.winning_score = winning_score
 
 def main():
     f = open("day4_input.txt")
     strings = f.readlines()
-
-    # === part 1 ===
 
     inputs = [int(x) for x in strings[0].split(',')]
     boards = []
@@ -97,22 +120,28 @@ def main():
                 boards.append( BingoBoard( tmp_brd.copy() ) )
                 tmp_brd = []
 
-    # print(len(boards))
-    # print(boards[0])
+    for board in boards:
+        board.calculate_win( inputs )
 
-    winning_score = 0
-    for val in inputs:
-        for board in boards:
-            board.mark_number( val )
-            if board.check_winner() == True:
-                print(board)
 
-                winning_score = board.get_winning_score( val )
-                break
+    # part 1
+    first_winner = boards[0]
+    for board in boards:
+        if board.number_of_moves_needed < first_winner.number_of_moves_needed:
+            first_winner = board
 
-    print(f"day4 part 1: {winning_score}")
+    print(f"moves to first win: {first_winner.number_of_moves_needed}")
+    print(f"day4 part 1: {first_winner.winning_score[0]}")
 
-    # === part 2 ===
+    moves_needed = 0
+    last_winner = None
+    for board in boards:
+        if board.number_of_moves_needed > moves_needed:
+            moves_needed = board.number_of_moves_needed
+            last_winner = board
+
+    print(f"moves to last win: {last_winner.number_of_moves_needed}")
+    print(f"day4 part 2: {last_winner.winning_score[0]}")
 
     return
 

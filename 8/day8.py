b4371e7c47c24d7a1b9d7058f28ee08a0f738d29
Sam Bukowski <sam.c.bukowski@gmail.com>
2021-12-15 21:21:51 -0700

    more day 8 progress

--- 8/day8.py
+++ 8/day8.py
@@ -17,116 +17,6 @@ def count_unique( data ):
                 count += 1
     return count
 
-def get_bar1(test):
-    collect = ""
-    for val in test:
-        if len(val) == 2:
-            collect += val
-        elif len(val) == 3:
-            collect += val
-    col = Counter([c for c in collect])
-    out = None
-    for k,v in col.items():
-        if v == 1:
-            out = k
-    return out
-
-def get_one(test):
-    one = ''
-    for val in test:
-        if len(val) == 2:
-            one = val
-    return one
-
-def get_bar2_and_bar4(test, one, bar1):
-    four = ''
-    five1 = ''
-    five2 = ''
-    five3 = ''
-    for val in test:
-        if len(val) == 4:
-            four = val
-        if len(val) == 5:
-            if not five1:
-                five1 = val
-                continue 
-            if not five2:
-                five2 = val
-                continue
-            if not five3:
-                five3 = val
-                continue
-
-    # print(four, five1, five2, five3)
-
-    four_o = four
-
-    col = Counter([c for c in four+five1+five2+five3])
-    bar4 = None
-    for k,v in col.items():
-        if v == 4:
-            bar4 = k
-
-    col = Counter([c for c in four+one+bar4])
-    bar2 = None
-    for k,v in col.items():
-        if v == 1:
-            bar2 = k
-
-    return bar2, bar4
-
-def get_bar7_and_five(test, one, bar1, bar2, bar4):
-    found = one+bar1+bar2+bar4
-    five1 = ''
-    five2 = ''
-    for val in test:
-        if len(val) == 5:
-            if not five1:
-                five1 = val
-                continue 
-            if not five2:
-                five2 = val
-                continue
-
-    five1_o = five1
-    five2_o = five2
-
-    for char in [c for c in found]:
-        five1 = five1.replace(char,"")
-        five2 = five2.replace(char,"")
-
-    five = ''
-    bar7 = ''
-    if len(five1) == 1:
-        bar7 = five1
-        five = five1_o
-    elif len(five2) == 1:
-        bar7 = five2
-        five = five2_o
-
-    return bar7, five
-
-def get_bar6(bar1, bar2, bar4, bar7, five):
-    val = bar1+bar2+bar4+bar7
-    for char in [c for c in val]:
-        five = five.replace(char,"")
-
-    return five
-
-def get_bar3(bar6,one):
-    return one.replace(bar6,"")
-
-def get_bar5(test, bar1, bar2, bar3, bar4, bar6, bar7):
-    eight = ''
-    for val in test:
-        if len(val) == 7:
-            eight = val
-
-    val = bar1+bar2+bar3+bar4+bar6+bar7
-    for char in [c for c in val]:
-        eight = eight.replace(char,"")    
-
-    return eight    
 
 class SevenSegment:
     def __init__(self, bar1, bar2, bar3, bar4, bar5, bar6, bar7):
@@ -137,6 +27,24 @@ class SevenSegment:
         self.bar5 = bar5
         self.bar6 = bar6
         self.bar7 = bar7
+
+        print(f" {self.bar1} ")
+        print(f"{self.bar2} {self.bar3}")
+        print(f" {self.bar4} ")
+        print(f"{self.bar5} {self.bar6}")
+        print(f" {self.bar7} ")
+
+
+    def build_zero(self):
+        output = ""
+        output += self.bar1
+        output += self.bar2
+        output += self.bar3
+        output += self.bar5
+        output += self.bar6
+        output += self.bar7
+        return output
+
 
     def build_one(self):
         output = ""
@@ -206,9 +114,21 @@ class SevenSegment:
         output += self.bar6
         output += self.bar7
         return output
+
+    def build_nine(self):
+        output = ""
+        output += self.bar1
+        output += self.bar2 
+        output += self.bar3 
+        output += self.bar4
+        output += self.bar6
+        output += self.bar7
+        return output
+
     
     def build_all_numbers(self):
         output = []
+        output.append( self.build_zero() )
         output.append( self.build_one() )
         output.append( self.build_two() )
         output.append( self.build_three() )
@@ -217,46 +137,85 @@ class SevenSegment:
         output.append( self.build_six() )
         output.append( self.build_seven() )
         output.append( self.build_eight() )
+        output.append( self.build_nine() )
         # numbers = [1,2,3,4,5,6,7]
         # return (numbers, output)
         return output
 
+def find_unique(test):
+    one = None
+    seven = None
+    four = None
+    eight = None
+    for val in test:
+        if len(val) == 2:
+            one = val
+        elif len(val) == 3:
+            seven = val
+        elif len(val) == 4:
+            four = val
+        elif len(val) == 7:
+            eight = val
+    
+    return one, four, seven, eight
+
+def get_len_five(test):
+    output = []
+    for val in test:
+        if len(val) == 5:
+            output.append(val)
+    return output
+
+def get_len_six(test):
+    output = []
+    for val in test:
+        if len(val) == 5:
+            output.append(val)
+    return output
+
+def get_three(len_fives, seven):
+    three = None
+    for val in len_fives:
+        intersection = [c for c in seven].intersect([c for c in val])
+        if len(intersection) == 3:
+            three = val
+    return three
 
 def determine_config(test):
-    bar5 = ''
-    bar6 = ''
+    zero = one = two = three = four = five = six = seven = eight = nine = None
+    one, four, seven, eight = find_unique(test)
+    len_fives = get_len_five(test)
+    len_sixs  = get_len_six(test)
+    three = get_three(len_fives, seven)
 
-    # print(test)
+    print(f"0 {zero}")
+    print(f"1 {one}")
+    print(f"2 {two}")
+    print(f"3 {three}")
+    print(f"4 {four}")
+    print(f"5 {five}")
+    print(f"6 {six}")
+    print(f"7 {seven}")
+    print(f"8 {eight}")
+    print(f"9 {nine}")
 
-    one = get_one(test)
-    bar1 = get_bar1(test)
-    bar2, bar4 = get_bar2_and_bar4(test, one, bar1)
-    bar7, five = get_bar7_and_five(test, one, bar1, bar2, bar4)
-    bar6 = get_bar6(bar1, bar2, bar4, bar7, five)
-    bar3 = get_bar3(bar6, one)
-    bar5 = get_bar5(test, bar1, bar2, bar3, bar4, bar6, bar7)
+
 
     session = SevenSegment(bar1, bar2, bar3, bar4, bar5, bar6, bar7)
-
-    # print(f" {bar1} ")
-    # print(f"{bar2} {bar3}")
-    # print(f" {bar4} ")
-    # print(f"{bar5} {bar6}")
-    # print(f" {bar7} ")
-
-    #  1-
-    # 2- 3-
-    #  4-
-    # 5 6-
-    #  7-
 
     return session
 
 def compare(res_item, thing):
+    arr = [ch for ch in res_item]
     count = 0
-    for c in res_item:
+    for c in arr:
         if c in thing:
             count += 1
+
+    if len(arr) == count:
+        return True
+    else:
+        return False
 
 
     # print("--",res_item, thing)
@@ -269,21 +228,64 @@ def compare(res_item, thing):
 
 def get_output_number(session, res):
     all_num = session.build_all_numbers()
+    # print(all_num[0])
+    # print(all_num[1])
+    # print(all_num[2])
+    # print(all_num[3])
+    # print(all_num[4])
+    # print(all_num[5])
+    # print(all_num[6])
+    # print(all_num[7])
+    # print(all_num[8])
+    # print(all_num[9])
+
     # print(all_num)
     # print(res)
     # print(len(res))
     # count = 0
     output = ""
     for number in res:
-        for n in all_num:
-            # print("*")
-            # print(n, number)
+        if len(number) == 2:
+            output += str(1)    # 1
+        elif len(number) == 3:
+            output += str(7)    # 7
+        elif len(number) == 4:
+            output += str(4)    # 4
+        elif len(number) == 5:
+            # 2
+            if compare(number,session.build_two()):
+                output += str(2)
+            # 3
+            elif compare(number, session.build_three()):
+                output += str(3)
+            # 5
+            elif compare(number, session.build_five()):
+                output += str(5)
+        elif len(number) == 6:
+            # 0
+            if compare(number, session.build_zero()):
+                output += str(0)
+            # 6
+            elif compare(number, session.build_six()):
+                output += str(6)
+            # 9
+            elif compare(number, session.build_nine()):
+                output += str(9)
+        elif len(number) == 7:
+            output += str(8)    # 8
+        else: 
+            pass
+            # for n in all_num:
+            #         # print("*")
+            #         # print(n, number)
 
-            if compare(n, number) and (len(n) == len(number)):
-                # print(n, number)
-                # print("*")
-                output += str(all_num.index(n))
+            #         if compare(n, number) and (len(n) == len(number)):
+            #             # print(n, number)
+            #             # print("*")
+            #             output += str(all_num.index(n))
+        break
         # count += 1
+    
     
     return output
 
